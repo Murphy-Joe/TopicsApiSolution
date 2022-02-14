38c3670d8c6e71a8e785b4743997ab602eb1ccb3
Jeffry Gonzalez <jeff@hypertheory.com>
2022-02-14 12:34:21 -0500

    Add project files.

---
+++ TopicsApi/Controllers/StatusController.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace TopicsApi.Controllers;
+
+public class StatusController : ControllerBase
+{
+
+    [HttpGet("status/oncalldeveloper")] 
+    public ActionResult GetOnCallDeveloper()
+    {
+        return Ok(); // this returns a 200 Ok status response.
+    }
+}
