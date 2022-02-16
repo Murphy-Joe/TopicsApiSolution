8c21c15d6b3a4c38f8ea3a0650db838750c73f5c
Jeffry Gonzalez <jeff@hypertheory.com>
2022-02-16 13:19:52 -0500

    before refactor

---
+++ TopicsApi/Services/RpcDeveloperLookup.cs
@@ -0,0 +1,20 @@
+﻿namespace TopicsApi.Services;
+
+public class RpcDeveloperLookup : ILookupOnCallDevelopers
+{
+    private readonly HttpClient _httpClient;
+
+    public RpcDeveloperLookup(HttpClient httpClient)
+    {
+        _httpClient = httpClient; // NEVER EVER EVER call "New" on the HTTPClient
+    }
+
+    public async Task<GetCurrentDeveloperModel> GetCurrentOnCallDeveloperAsync()
+    {
+        var response = await _httpClient.GetAsync("/");
+
+        var content = await response.Content.ReadFromJsonAsync<GetCurrentDeveloperModel>();
+
+        return content!;
+    }
+}
